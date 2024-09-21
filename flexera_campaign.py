563eada715e4f1a15366f94b55c7b891d9adc3d1
Speedy <148458995+speedyownjoo@users.noreply.github.com>
2024-09-21 14:19:41 -0500

    add methods for SOAP and REST

--- flexera_campaign.py
+++ flexera_campaign.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import sys
 
 from json import loads, dumps
 from typing import Optional, Generator
@@ -7,14 +8,80 @@ from typing import Optional, Generator
 import http.client
 
 from requests import HTTPError, Session, Response
+from zeep import Client, Transport, xsd
 
 http.client.HTTPConnection.debuglevel = 0  # 0 for off, >0 for on
 
 log_level: int = logging.ERROR
 logging.basicConfig()
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(log_level)
-requests_log.propagate = True
+logger = logging.getLogger("requests.packages.urllib3")
+logger.setLevel(log_level)
+logger.propagate = True
+
+
+def modify_retire_campaign_rest(
+        session: Session,
+        domain: str,
+        flexera_id: str = '',
+        group_id: str = '',
+) -> None:
+    try:
+        url: str = f'{domain}/esd/api/Campaigns'
+        data: list = [
+            {
+                'campaignType': 3,
+                'flexeraId': flexera_id,
+                'propertyType': 'Group',
+                'propertyName': 'memberOf',
+                'propertyValue': group_id,
+                'visible': True,
+                'applyChildOU': False,
+                'propertyDisplayName': group_id,
+            },
+        ]
+        resp_campaign: Response = session.post(
+            url=url,
+            params={'flexeraid': flexera_id},
+            data=data,
+        )
+        print(resp_campaign.text)
+    except HTTPError as http_error:
+        msg: str = f'''WSDL RESPONSE: {http_error}:
+        STATUS: {http_error.response.status_code}
+        HEADERS: {http_error.request.headers}
+        BODY: {http_error.request.body}'''
+        print(msg, file=sys.stderr)
+        logger.exception(http_error)
+    except Exception as exc_campaign_rest:
+        logger.exception(exc_campaign_rest)
+
+
+def create_retire_campaign_soap(
+        session: Session,
+        domain: str,
+        flexera_id: str = '',
+) -> None:
+    url: str = f'{domain}/esd/ws/integration.asmx'
+    wsdl: Optional[str] = None
+    try:
+        resp_wsdl: Response = session.get(url=f'{url}?WSDL')
+        resp_wsdl.raise_for_status()
+        wsdl: str = resp_wsdl.text
+        logger.debug(f'WSDL: {wsdl}')
+    except HTTPError as http_error_wsdl:
+        msg: str = f'''WSDL RESPONSE: {http_error_wsdl}:
+        STATUS: {http_error_wsdl.response.status_code}
+        HEADERS: {http_error_wsdl.request.headers}
+        BODY: {http_error_wsdl.request.body}'''
+        print(msg, file=sys.stderr)
+        logger.exception(http_error_wsdl)
+    except Exception as exc_wsdl:
+        logger.exception(f'Error getting WSDL: {exc_wsdl}')
+        raise exc_wsdl
+
+    client = Client(wsdl, transport=Transport(session=session._session))
+    resp_campaign: xsd.CompoundValue = client.service.AddFlexeraIdForRetireCampaign(flexera_id)
+    logger.debug(f'CAMPAIGN: {resp_campaign}')
 
 
 def create_campaign(
@@ -35,9 +102,9 @@ def create_campaign(
         devices: list = data.get('data')
         yield from devices
     except HTTPError as exc_http:
-        requests_log.error(f'{exc_http}: {exc_http.response.request.headers}')
+        logger.error(f'{exc_http}: {exc_http.response.request.headers}')
     except Exception as exc_dev:
-        requests_log.error(exc_dev)
+        logger.error(exc_dev)
 
 
 def main(
